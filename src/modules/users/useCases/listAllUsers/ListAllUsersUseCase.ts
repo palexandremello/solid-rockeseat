d12923a8d8b0fe3276729c7c5faf1ffb578c0b2b
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-12-11 23:59:46 -0300

    feat: ListAllUsersUseCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+
+    if (!user.admin) {
+      throw new Error("Should not able to list users without admin permission");
+    }
+
+    return this.usersRepository.list()
   }
 }
 
